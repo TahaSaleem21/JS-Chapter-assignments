9087aab4e8f84fec6b7c038a0ac271f16db832f8
Taha Saleem <tstahasaleem@gmail.com>
2024-03-22 18:20:56 -0700

    first and second chapter

---
+++ chapter 2/app.js
@@ -0,0 +1,53 @@
+////----Question-1----////
+
+let username;
+
+////----Question-2----////
+
+let myname = "Taha Saleem";
+
+////----Question-3----////
+
+let message = "Hello World";
+alert(message);
+
+////----Question-4----////
+
+let student_name = prompt("Your Name");
+let student_age = prompt("Your Age");
+let student_course = prompt("Your Course");
+alert(
+  "Student Name: " +
+    student_name +
+    "\n" +
+    "Student Age: " +
+    student_age +
+    "\n" +
+    "Student Course: " +
+    student_course
+);
+
+////----Question-5----////
+
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+////----Question-6----////
+
+let email = "google@gmail.com";
+alert("Enter email " + email);
+
+////----Question-7----////
+
+let book = "A Smarter way to Learn JavaScript";
+alert(
+  "I'm trying to learn from the Book A Smarter way to Learn JavaScript" + book
+);
+
+////----Question-8----////
+
+document.write("Yah! I can write HTML content through Javascript");
+
+////----Question-9----////
+
+let END = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(END);
---
+++ chapter 4/app.js
@@ -0,0 +1,49 @@
+////----Question-1----////
+
+let variable1 = 21,
+  variable2 = "Javascript",
+  variable3 = true;
+
+////----Question-2----////
+
+//--Legal variables--//
+
+let age;
+let firstName;
+let _lastname;
+let $name;
+let userEmail;
+
+//--Illegal variables--//
+
+//let 123age;
+//let var;
+//let user-email;
+//let user age;
+//let @username;
+
+////----Question-3----////
+
+//----------A----------//
+document.getElementById("myHeading").textContent =
+  "Rules for naming JS variables";
+
+//----------B----------//
+
+document.write(
+  "Variable names can only contain , numbers, $ and _. For example : $my_1stVariable <br>"
+);
+
+//----------C----------//
+
+document.write(
+  "Variable must begin with a letter, $ or _. For example: $name, _name or name <br>"
+);
+
+//----------D----------//
+
+document.write("Variable names are case (SENSITIVE) <br>");
+
+//----------E----------//
+
+document.write("Variable names should not be JS (KEYWORDS)");
